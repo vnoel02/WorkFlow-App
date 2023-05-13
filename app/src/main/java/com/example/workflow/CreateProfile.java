884dae80b126c8b1dc1d5f22dba98fa03bf887a5
Victor Noel <vnoel890@gmail>
2023-05-13 14:18:43 -0400

    a little cleaner code

--- app/src/main/java/com/example/workflow/CreateProfile.java
+++ app/src/main/java/com/example/workflow/CreateProfile.java
@@ -2,6 +2,7 @@ package com.example.workflow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -9,8 +10,13 @@ import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class CreateProfile extends AppCompatActivity {
+    EditText et;
+    EditText et2;
+    EditText et3;
+    EditText et4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,56 +26,55 @@ public class CreateProfile extends AppCompatActivity {
 
     }
 
+
+
     public void saveProfileOnClick() {
         Button profile = findViewById(R.id.create_profile);
 
-        EditText et = findViewById(R.id.first_name);
-        EditText et2 = findViewById(R.id.last_name);
-        EditText et3 = findViewById(R.id.user_name);
-        EditText et4 = findViewById(R.id.email);
+        et = findViewById(R.id.first_name);
+        et2 = findViewById(R.id.last_name);
+        et3 = findViewById(R.id.user_name);
+        et4 = findViewById(R.id.email);
 
         profile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                String firstName = et.getText().toString();
-                String lastName = et2.getText().toString();
-                String username = et3.getText().toString();
-                String email = et4.getText().toString();
+                if (missingInfo()) {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Please enter missing info";
+                    int duration = Toast.LENGTH_SHORT;
 
-                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(CreateProfile.this);
-                SharedPreferences.Editor editor = prefs.edit();
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                } else {
+                    String firstName = et.getText().toString();
+                    String lastName = et2.getText().toString();
+                    String username = et3.getText().toString();
+                    String email = et4.getText().toString();
 
-                editor.putString("firstName", firstName);
-                editor.putString("lastName", lastName);
-                editor.putString("username", username);
-                editor.putString("email", email);
+                    SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(CreateProfile.this);
+                    SharedPreferences.Editor editor = prefs.edit();
 
-                //Intent intent = new Intent(com.example.workflow.CreateProfile.this, MainActivity.class);
-                Intent intent2 = new Intent(com.example.workflow.CreateProfile.this, ProfileViewActivity.class);
+                    editor.putString("firstName", firstName);
+                    editor.putString("lastName", lastName);
+                    editor.putString("username", username);
+                    editor.putString("email", email);
 
-
-//                String firstName = et.getText().toString();
-//                intent2.putExtra("FirstName", firstName);
-//
-//                String lastName = et2.getText().toString();
-//                intent2.putExtra("LastName",lastName);
-//
-//                String username = et3.getText().toString();
-//                intent2.putExtra("Username", username);
-//
-//                String email = et4.getText().toString();
-//                intent2.putExtra("Email", email);
-
-                editor.apply();
-                Global s = new Global();
-                s.createdProfile = true;
-                //startActivity(intent);
-                startActivity(intent2);
-
+                    Intent intent = new Intent(com.example.workflow.CreateProfile.this, MainActivity.class);
+                    editor.apply();
+                    Global s = new Global();
+                    s.createdProfile = true;
+                    startActivity(intent);
+                }
             }
         });
     }
 
+
+    public boolean missingInfo() {
+        return et.getText().toString().equals("") || et2.getText().toString().equals("")
+                || et3.getText().toString().equals("") || et4.getText().toString().equals("");
+    }
 
 }
--- app/src/main/java/com/example/workflow/ProfileViewActivity.java
+++ app/src/main/java/com/example/workflow/ProfileViewActivity.java
@@ -17,17 +17,7 @@ public class ProfileViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_view);
 
-        /*SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPref.edit();
-        TextView tv = findViewById(R.id.receive_first);
-        TextView tv2 = findViewById(R.id.receive_second);
-        String firstName = sharedPref.getString("FirstName", "");
-        String secondName = sharedPref.getString("SecondName","");
-        tv.setText(firstName);
-        tv2.setText(secondName);*/
-
         final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
-        //SharedPreferences.Editor editor = prefs.edit();
 
         String firstName = prefs.getString("firstName", "");
         TextView tv = findViewById(R.id.text_firstname);
@@ -45,31 +35,10 @@ public class ProfileViewActivity extends AppCompatActivity {
         TextView tv4 = findViewById(R.id.profile_email);
         tv4.setText(email);
 
-
-//        String firstName = getIntent().getStringExtra("FirstName");
-//        final TextView tv = findViewById(R.id.text_firstname);
-//        tv.setText(firstName);
-//
-//        String lastName = getIntent().getStringExtra("LastName");
-//        final TextView tv2 = findViewById(R.id.text_lastName);
-//        tv2.setText(lastName);
-//
-//        String username = getIntent().getStringExtra("Username");
-//        final TextView tv3 = findViewById(R.id.profile_username);
-//        tv3.setText(username);
-//
-//        String email = getIntent().getStringExtra("Email");
-//        final TextView tv4 = findViewById(R.id.profile_email);
-//        tv4.setText(email);
-
-        //editor.apply();
     }
 
     public void returnOnClick(View view) {
         Intent intent = new Intent(ProfileViewActivity.this, MainActivity.class);
         startActivity(intent);
-
     }
-
-
 }
