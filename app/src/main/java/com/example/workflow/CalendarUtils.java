77a3eceb2aa7c73b96f47f8e7d9635fc72ff4b9e
rodjean1234 <rodjean1234@gmail.com>
2023-05-15 00:37:16 -0400

    Added event placing functionality

---
+++ app/src/main/java/com/example/workflow/CalendarUtils.java
@@ -0,0 +1,74 @@
+package com.example.workflow;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+public class CalendarUtils
+{
+    public static LocalDate selectedDate;
+
+    public static String formattedDate(LocalDate date)
+    {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy");
+        return date.format(formatter);
+    }
+
+    public static String formattedTime(LocalTime time)
+    {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm:ss a");
+        return time.format(formatter);
+    }
+
+    public static String formattedShortTime(LocalTime time)
+    {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+        return time.format(formatter);
+    }
+
+    public static String monthYearFromDate(LocalDate date)
+    {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
+        return date.format(formatter);
+    }
+
+    public static String monthDayFromDate(LocalDate date)
+    {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM d");
+        return date.format(formatter);
+    }
+
+    public static ArrayList<LocalDate> daysInMonthArray()
+    {
+        ArrayList<LocalDate> daysInMonthArray = new ArrayList<>();
+
+        YearMonth yearMonth = YearMonth.from(selectedDate);
+        int daysInMonth = yearMonth.lengthOfMonth();
+
+        LocalDate prevMonth = selectedDate.minusMonths(1);
+        LocalDate nextMonth = selectedDate.plusMonths(1);
+
+        YearMonth prevYearMonth = YearMonth.from(prevMonth);
+        int prevDaysInMonth = prevYearMonth.lengthOfMonth();
+
+        LocalDate firstOfMonth = CalendarUtils.selectedDate.withDayOfMonth(1);
+        int dayOfWeek = firstOfMonth.getDayOfWeek().getValue();
+
+        for(int i = 1; i <= 42; i++)
+        {
+            if(i <= dayOfWeek)
+                daysInMonthArray.add(LocalDate.of(prevMonth.getYear(),prevMonth.getMonth(),prevDaysInMonth + i - dayOfWeek));
+            else if(i > daysInMonth + dayOfWeek)
+                daysInMonthArray.add(LocalDate.of(nextMonth.getYear(),nextMonth.getMonth(),i - dayOfWeek - daysInMonth));
+            else
+                daysInMonthArray.add(LocalDate.of(selectedDate.getYear(),selectedDate.getMonth(),i - dayOfWeek));
+        }
+        return  daysInMonthArray;
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/workflow/EventEditActivity.java
@@ -0,0 +1,67 @@
+package com.example.workflow;
+
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.time.LocalTime;
+import java.util.Locale;
+
+public class EventEditActivity extends AppCompatActivity
+{
+    private EditText eventNameET;
+    private TextView eventDateTV, eventTimeTV;
+
+    private LocalTime time;
+
+    Button timeButton;
+    int hour, minute;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_edit);
+        initWidgets();
+        time = LocalTime.now();
+        eventDateTV.setText("Date: " + CalendarUtils.formattedDate(CalendarUtils.selectedDate));
+        eventTimeTV.setText("Time: " + CalendarUtils.formattedTime(time));
+        timeButton = findViewById(R.id.eventTimeTV);
+    }
+    public void popTimePicker(View view){
+        TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                hour = selectedHour;
+                minute = selectedMinute;
+                timeButton.setText(String.format(Locale.getDefault(), "%02d:%02d",hour, minute));
+                LocalTime k  = LocalTime.parse("03:10");
+            }
+        };
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, /*style,*/ onTimeSetListener, hour, minute, true);
+
+        timePickerDialog.setTitle("Select Time");
+        timePickerDialog.show();
+    }
+    private void initWidgets()
+    {
+        eventNameET = findViewById(R.id.eventNameET);
+        eventDateTV = findViewById(R.id.eventDateTV);
+        eventTimeTV = findViewById(R.id.eventTimeTV);
+    }
+
+    public void saveEventAction(View view)
+    {
+        String eventName = eventNameET.getText().toString();
+        time = LocalTime.parse(timeButton.getText().toString());
+        Event newEvent = new Event(eventName, CalendarUtils.selectedDate, time);
+        Event.eventsList.add(newEvent);
+        finish();
+    }
+}
